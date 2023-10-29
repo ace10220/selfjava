b24c559c9885ec35f2d55cfa0d09103b84a53990
ace10220 <nagaitomoya10220@gmail.com>
2023-10-29 14:53:54 +0900

    ArrayJagged.java（リスト2.2）を初期化

---
+++ src/selfjava/com/ace10220/chap02/ArrayJagged.java
@@ -0,0 +1,13 @@
+package com.ace10220.chap02;
+
+public class ArrayJagged {
+    public static void main(String[] args) {
+        var list = new int[][] {
+            { 1, 2 },
+            { 3, 4, 5, 6 },
+            { 7, 8, 9 },
+        };
+
+        System.out.println(list[1][2]);
+    }
+}
