3b5bbb5dd78f920256b3d36c3c0fa293489a562d
ace10220 <nagaitomoya10220@gmail.com>
2023-10-29 14:13:36 +0900

    Const.java（リスト2.1）を初期化

---
+++ src/selfjava/com/ace10220/chap02/Const.java
@@ -0,0 +1,10 @@
+package com.ace10220.chap02;
+
+public class Const {
+    public static void main(String[] args) {
+        final double TAX = 1.08;
+        int price = 1000;
+        double sum = price * TAX;
+        System.out.println(sum);
+    }
+}
