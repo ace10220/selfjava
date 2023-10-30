3c78b86287e7ce03a1a022343bc32695f254bca3
ace10220 <nagaitomoya10220@gmail.com>
2023-10-30 13:58:50 +0900

    Increment.java（リスト3.4）を初期化

---
+++ src/selfjava/com/ace10220/chap03/Increment.java
@@ -0,0 +1,15 @@
+package com.ace10220.chap03;
+
+public class Increment {
+    public static void main(String[] args) {
+        var i = 3;
+        var j = ++i;
+        System.out.println(i);
+        System.out.println(j);
+
+        var m = 3;
+        var n = m++;
+        System.out.println(m);
+        System.out.println(n);
+    }
+}
